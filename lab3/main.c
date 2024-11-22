c44f5887ff3608e674b09edcf7737647c3bb8954
Ahmed <144798780+ahmedoweis72@users.noreply.github.com>
2024-11-22 14:47:15 +0200

    Update main.c
    fixed some bugs

--- lab3/main.c
+++ lab3/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #define SIZE 100
 
 #include "conio.h"
@@ -22,7 +21,7 @@
 #define END 70
 #define ONE 49
 #define ZERO 48
-
+#define TERMINATOR '\0'
 
 typedef struct Employee {
     char name[10];
@@ -36,7 +35,7 @@ int main()
 {
 
     Employee employees[SIZE] = {{employees->code=0}};
-   char  ch,flag=1,option=0,posi=0,chkKEy=0;
+   char  ch,flag=1,option=0,posi=0,chkKEy=0,exit=0;;
     while(flag==1){
 
 int id=0;
@@ -90,8 +89,9 @@ int id=0;
 
                         while (new==1)
                             {
-                                    Employee e;
+                                    
                                 for (int i = 0; i < SIZE; i++){
+                                    Employee e;
                                 if (employees[i].code==0){
                                 e.code=i+1;
                                 printf("Enter employee name: \n");
@@ -122,9 +122,22 @@ int id=0;
                                         }
                                     }
                                     printf("If you want to add another employee, press 1. If not, press 0.\n");
-                                    new=0;
+                                    scanf("%d",&newEmployee);
+                                    if(newEmployee==0){
+                                        new=0;
+                                        exit=1;
+                                        posi=2;
+                                        option=7;
+                                        
+                                        
+                                        }
+                                        else
+                                        {
+                                            new=1;
+                                            }
+                                    
 
-                                    ch=getch();
+                                    
                             }
 
 
@@ -151,6 +164,7 @@ int id=0;
                         }else if(posi==2) {
                                 printf("click backspace or Esc to back\n");
                                 printf(" \t\t\n\n\n\t%snew%s\n\n\n ",BLUE,RESET);
+                                exit=0;
                         }else if(posi==4) {
                                 printf("click enter to exit\n");
                                 printf("\t\t\t exit \n\n\n");
@@ -160,12 +174,12 @@ int id=0;
                                 printf("chose option Record to edit\n");
                                 printf("%s\t\t\t Modify \n\n\n%s",BLUE,RESET);
 
-
+                                
                         }
 
                     break;
             case 8:clear();
-
+                            
                         printf("%schose option Record to edit%s\n",BLUE,RESET);
                         for (int i = 0; i < SIZE; i++)
                         {
@@ -173,30 +187,58 @@ int id=0;
                             {
                                 printf("Employee Id %d \n",employees[i].code);
                             }
-
+                            
                         }
+                        if (employees[0].code==0)
+                        {
+                            printf("No Data\n");
+                           option=7;
+                           posi=3;
+                           
+                            
+
+                        }else{
+                        
                         printf("Enter the Employee ID to edit\n");
-                        scanf("%d",&id);
+                        scanf("%d",&id);}
+                        if (id>0)
+                        {
+                            id=id-1;
+                        }
                         break;
             case 9:clear();
-                        char name[50];
-                        int netSalary;
-                        printf("%schose option Record to edit%s\n",BLUE,RESET);
-                        printf("Employee Id %d \n",employees[id-1].code);
-                                printf("Employee Name %s \n",employees[id-1].name);
-                                printf("Employee NetSalary %d \n",employees[id-1].netSalary);
+                            char name[10];
+                            int netSalary,editAgain=0;
+                            printf("%schose option Record to edit%s\n", BLUE, RESET);
+                            printf("Employee Id %d \n", employees[id].code);
+                            printf("Employee Name %s \n", employees[id].name);
+                            printf("Employee NetSalary %d \n", employees[id].netSalary);
+                            printf("Enter the Employee Name to edit\n");
+                            scanf("%s", name); 
+                            printf("Enter the Employee NetSalary to edit\n");
+                            scanf("%d", &netSalary);
+                            strcpy(employees[id].name, name); 
+                            employees[id].netSalary = netSalary;
+                            printf("Edited\n");
+                            printf("If you want to edit again press 1\n");
+                            scanf("%d", &editAgain);
+                            if (editAgain==1)
+                            {
+                                option=9;
+                            }else 
+                            
+                            {   
+                                option=8;
+                            }
+                            
+                            
+                        
+                            
+                            
 
-
-                                printf("Enter the Employee Name to edit\n");
-                                scanf("%s",&name);
-                                printf("Enter the Employee NetSalary to edit\n");
-                                scanf("%d",&netSalary);
-                                employees[id-1].name=name;
-                                employees[id-1].netSalary=netSalary;
-
-
-
-                        break;
+                        
+                        break;  
+      
         default:clear();
                     printf("%schoise option  up&down %s\n", BLUE, RESET);
                     printf("\t\t\t1-Display\n\n\n");
@@ -204,6 +246,7 @@ int id=0;
                     printf("\t\t\t3-Modifay\n\n\n");
                     printf("\t\t\t4-exit\n\n\n");
                     break;
+                   
 
 
 
@@ -212,13 +255,13 @@ int id=0;
 
 
              ch=getch();
-
+            
          if(ch == EXTENDKEY){
-         if(kbhit()==0)
-         { if (ch==ESC){
+         if(kbhit()==0&&ch==ESC)
+         { 
             option=posi;
             posi=0;
-         }
+         
 
             }else{
 
@@ -261,9 +304,13 @@ int id=0;
             option=6;
          }
          else if(ch==ENTERKEY&&posi==2&&option==7)
+         {  if (exit==0)
          {
-
             option=5;
+         }
+         
+
+            
          }
          else if (ch==ZERO&&option==5){
             option=7;
@@ -278,15 +325,15 @@ int id=0;
          }else if (ch==ENTERKEY&&posi==3)
          {  posi=option;
             option=8;
-         } else if (ch==ENTERKEY&&option==8)
+         } else if (ch==ENTERKEY&&option==8&&employees[0].code!=0)
          {
             option=9;
          }
-
-
-
-
-
+         
+         
+         
+        
+         
 
 
     }
